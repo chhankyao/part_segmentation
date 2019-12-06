29ef92bad570dc45b92da25458c783ddadd06c18
Chun-Han Yao <chy235@eng.ucsd.edu>
2019-12-06 11:26:16 -0800

    update transformation

--- utils.py
+++ utils.py
@@ -34,21 +34,18 @@ def transforms_pam(inputs, flip, angle, translate, scale, shear):
 
 
 def transform_img(inputs, flip, angle, translate, scale, shear):
+    nb, nc, h, w = [int(s) for s in inputs.size()]
+    h2, w2 = int(round(h*scale)), int(round(w*scale))
     theta = torch.zeros(1, 2, 3)
     theta[:, :, :2] = torch.tensor([[np.cos(angle), -np.sin(angle)],
                                     [np.sin(angle), np.cos(angle)]])
     theta[:, :, 2] = torch.tensor(translate)
-
-    nb, nc, h, w = [int(s) for s in inputs.size()]
-    h2, w2 = int(round(h*scale)), int(round(w*scale))
+    theta = theta.repeat(nb, 1, 1)
     grid = F.affine_grid(theta, (nb, nc, h2, w2), align_corners=True)
     inputs_tf = F.grid_sample(inputs, grid, padding_mode="border")
-
     x1 = int(round((w2 - w) / 2.))
     y1 = int(round((h2 - h) / 2.))
     input_tf = inputs_tf[:, :, y1:y1+h, x1:x1+w]
-
     if flip:
         inputs_tf = inputs_tf.flip([3])
-
     return inputs_tf
